39b01d8f422b9878ef881006a0d0eeda6efbfa87
efoejean <efoejean@gmail.com>
2022-01-09 19:02:32 -0600

    npm i @material-ui create nav components

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import { AppBar, Typography } from "@material-ui/core";
+import React from "react";
+
+const Nav = () => (
+  <AppBar position="static" color="inherit">
+    <div>
+      <Typography variant="h6"> Welcome</Typography>
+    </div>
+  </AppBar>
+);
+
+export default Nav;
