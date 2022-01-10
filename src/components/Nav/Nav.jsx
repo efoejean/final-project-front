65d94e033bb80ffcb743206dd691ff6a35b8dc30
efoejean <efoejean@gmail.com>
2022-01-09 19:30:52 -0600

    Using ternary to create user toolbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,35 @@
-import { AppBar, Typography } from "@material-ui/core";
+import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Nav = () => (
-  <AppBar position="static" color="inherit">
-    <div>
-      <Typography variant="h6"> Welcome</Typography>
-    </div>
-  </AppBar>
-);
+const Nav = () => {
+  const user = null;
+
+  return (
+    <AppBar position="static" color="inherit">
+      <div>
+        <Typography component={Link} to="/" variant="h6">
+          {" "}
+          Welcome
+        </Typography>
+      </div>
+      <Toolbar>
+        {user ? (
+          <div className="">
+            <Avatar alt={user.result.name} src={user.result.imageUrl}>
+              user.name.charAt(0)
+            </Avatar>
+            <Typography className="" variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button variant="contained" color="secondary">
+              Logout
+            </Button>
+          </div>
+        ) : null}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Nav;
