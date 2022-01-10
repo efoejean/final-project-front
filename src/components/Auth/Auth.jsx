739d962ce5ec5116da839abb991055caa8241d47
efoejean <efoejean@gmail.com>
2022-01-09 20:53:13 -0600

    Create a first part of the sign form

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,5 +1,26 @@
+import { Avatar, Container, Paper, Typography } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import React from "react";
 
-const Auth = () => <div>Auth</div>;
+const Auth = () => {
+  const isSignup = false;
+  return (
+    <Container maxWidth="sm">
+      <Paper className="p-8 m-8 rounded-sm">
+        <Avatar
+          className="m-8"
+          alt="Remy Sharp"
+          src="https://source.unsplash.com/random"
+        >
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          {isSignup ? "Sign Up" : "Sign In"}
+        </Typography>
+        <form className=""></form>
+      </Paper>
+    </Container>
+  );
+};
 
 export default Auth;
