1a4a36a7d0c9b4e79db8c4a5ac708f913a0b6b31
efoejean <efoejean@gmail.com>
2022-01-09 20:16:10 -0600

    Import Auth,Home, Nav and Routes to  route our app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { Container } from "@material-ui/core";
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+import Home from "./components/Home/Home";
 import Nav from "./components/Nav/nav";
 
-function App() {
-  return <Nav />;
-}
+const App = () => (
+  <BrowserRouter>
+    <Container>
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </Container>
+  </BrowserRouter>
+);
 
 export default App;
