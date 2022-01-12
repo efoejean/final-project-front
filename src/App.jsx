b077fb35000acc00cbbb39130a44c76679d4f6af
efoejean <efoejean@gmail.com>
2022-01-11 23:32:57 -0600

    Add customer routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Container } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "./components/Auth/Auth";
+import Customers from "./components/Home/Customers";
 import Home from "./components/Home/Home";
 import Nav from "./components/Nav/nav";
 
@@ -13,6 +14,7 @@ const App = () => (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/customers" element={<Customers />} />
       </Routes>
     </Container>
   </BrowserRouter>
