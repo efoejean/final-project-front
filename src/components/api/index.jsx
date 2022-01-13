55e84bee92821f74caac6ca027ea72472f02e440
efoejean <efoejean@gmail.com>
2022-01-12 18:22:48 -0600

    Add async and await to get the tooken value

--- src/components/api/index.jsx
+++ src/components/api/index.jsx
@@ -3,24 +3,27 @@ import axios from "axios";
 // create a base url
 const API = axios.create({ baseURL: "http://localhost:4000/api" });
 
-// make a request to the server and send the formData.
-const signin = (formData) =>
-  API.post("/admin/login", formData).then((res) => {
-    if (res.data.jwt) {
+// make a request to the server and send the formData. i was missing async and await
+const signin = async (formData) => {
+  await API.post("/admin/login", formData).then((res) => {
+    if (res.data) {
       localStorage.setItem("user", JSON.stringify(res.data));
     }
 
     return res.data;
   });
+};
 
-const signup = (formData) =>
-  API.post("/admin/register", formData).then((res) => {
-    if (res.data.jwt) {
+// make a request to the server and send the formData to sign up
+const signup = async (formData) => {
+  await API.post("/admin/register", formData).then((res) => {
+    if (res.data) {
       localStorage.setItem("user", JSON.stringify(res.data));
     }
 
     return res.data;
   });
+};
 
 const logout = () => {
   localStorage.removeItem("user");
