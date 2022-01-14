c90c817bf6c77b87aafab0a055a43665a96d1f3e
efoejean <efoejean@gmail.com>
2022-01-13 21:22:54 -0600

    create  a base url for the backend with axios and the token
    use the base url and the token to get the data, use async to wait for the data to be returned

--- src/components/Home/Customers.jsx
+++ src/components/Home/Customers.jsx
@@ -1,30 +1,42 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
-import { getService } from "../api/getService";
+import { useNavigate } from "react-router-dom";
+
+// create  a base url for the backend with axios and the token
+const authAxios = axios.create({
+  baseURL: "http://localhost:4000/api/",
+  headers: {
+    Authorization: `Bearer ${JSON.parse(localStorage.getItem("user"))}`,
+  },
+});
 
 const Customers = () => {
   const [customers, setCustomers] = useState([]);
 
+  const navigate = useNavigate();
   // use effect to get the data from the backend and send it the variable customers
   useEffect(() => {
-    getService().then(
-      (res) => {
+    // use the base url and the token to get the data, use async to wait for the data to be returned
+    (async () => {
+      try {
+        const res = await authAxios.get("/customers");
         setCustomers(res.data);
-      },
-      (err) => {
-        console.log(err);
+      } catch (error) {
+        console.log(error);
       }
-    );
+    })();
   }, []);
 
   // map through the customers and return the data
   return (
-    <div>
-      <h3>
-        {customers.map((customer) => (
-          <div key={customer.id}>{customer.name}</div>
-        ))}
-      </h3>
-    </div>
+    <ul>
+      $
+      {customers.map((customer) => (
+        <li key={customer.id}>
+          <span>${customer.name}</span>
+        </li>
+      ))}
+    </ul>
   );
 };
 
